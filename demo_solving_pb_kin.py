6ef99d8299a3187f772bdacb29eeb4a29595afe7
arthur <arthur.haffemayer@yahoo.fr>
2023-10-25 10:58:22 +0200

    black formatting

--- demo_solving_pb_kin.py
+++ demo_solving_pb_kin.py
@@ -1,37 +1,34 @@
 import os
 import sys
- 
+
 import crocoddyl
 import pinocchio as pin
 import numpy as np
 import example_robot_data
 from wrapper_meshcat import MeshcatWrapper
- 
+
 WITHDISPLAY = False
 WITHPLOT = False
- 
+
 # In this example test, we will solve the reaching-goal task with the Talos arm.
 # For that, we use the forward dynamics (with its analytical derivatives)
 # developed inside crocoddyl; it describes inside DifferentialActionModelFullyActuated class.
 # Finally, we use an Euler sympletic integration scheme.
- 
+
 # First, let's load the Pinocchio model for the Talos arm.
-robot = example_robot_data.load('ur10')
- 
+robot = example_robot_data.load("ur10")
+
 rmodel = robot.model
 vmodel = robot.visual_model
 cmodel = robot.collision_model
 
-TARGET = pin.SE3(np.eye(3), np.array([.7, .5, .4]))
+TARGET = pin.SE3(np.eye(3), np.array([0.7, 0.5, 0.4]))
 INITIAL_CONFIG = pin.neutral(rmodel)
 
 # Generating the meshcat visualizer
 MeshcatVis = MeshcatWrapper()
 vis = MeshcatVis.visualize(
-    TARGET,
-    robot_model=rmodel,
-    robot_collision_model=cmodel,
-    robot_visual_model=vmodel
+    TARGET, robot_model=rmodel, robot_collision_model=cmodel, robot_visual_model=vmodel
 )
 vis = vis[0]
 
@@ -44,14 +41,15 @@ vis.display(INITIAL_CONFIG)
 state = crocoddyl.StateMultibody(rmodel)
 runningCostModel = crocoddyl.CostModelSum(state)
 terminalCostModel = crocoddyl.CostModelSum(state)
- 
+
 # # Note that we need to include a cost model (i.e. set of cost functions) in
 # # order to fully define the action model for our optimal control problem.
 # # For this particular example, we formulate three running-cost functions:
 # # goal-tracking cost, state and control regularization; and one terminal-cost:
 # # goal cost. First, let's create the common cost functions.
-framePlacementResidual = crocoddyl.ResidualModelFramePlacement(state, rmodel.getFrameId("tool0"), TARGET
-                                                               )
+framePlacementResidual = crocoddyl.ResidualModelFramePlacement(
+    state, rmodel.getFrameId("tool0"), TARGET
+)
 
 
 uResidual = crocoddyl.ResidualModelControl(state)
@@ -59,25 +57,33 @@ xResidual = crocoddyl.ResidualModelControl(state)
 goalTrackingCost = crocoddyl.CostModelResidual(state, framePlacementResidual)
 xRegCost = crocoddyl.CostModelResidual(state, xResidual)
 uRegCost = crocoddyl.CostModelResidual(state, uResidual)
- 
+
 # # Then let's added the running and terminal cost functions
 runningCostModel.addCost("gripperPose", goalTrackingCost, 1)
 runningCostModel.addCost("xReg", xRegCost, 1e-4)
 runningCostModel.addCost("uReg", uRegCost, 1e-4)
 terminalCostModel.addCost("gripperPose", goalTrackingCost, 1)
- 
+
 # # Next, we need to create an action model for running and terminal knots. The
 # # forward dynamics (computed using ABA) are implemented
 # # inside DifferentialActionModelFullyActuated.
 actuationModel = crocoddyl.ActuationModelFull(state)
 dt = 1e-3
 runningModel = crocoddyl.IntegratedActionModelEuler(
-    crocoddyl.DifferentialActionModelFreeFwdDynamics(state, actuationModel, runningCostModel), dt)
+    crocoddyl.DifferentialActionModelFreeFwdDynamics(
+        state, actuationModel, runningCostModel
+    ),
+    dt,
+)
 runningModel.differential.armature = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
 terminalModel = crocoddyl.IntegratedActionModelEuler(
-    crocoddyl.DifferentialActionModelFreeFwdDynamics(state, actuationModel, terminalCostModel), 0.)
+    crocoddyl.DifferentialActionModelFreeFwdDynamics(
+        state, actuationModel, terminalCostModel
+    ),
+    0.0,
+)
 terminalModel.differential.armature = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
- 
+
 # # For this optimal control problem, we define 1000 knots (or running action
 # # models) plus a terminal knot
 T = 1000
@@ -85,18 +91,20 @@ T = 1000
 q0 = INITIAL_CONFIG
 x0 = np.concatenate([q0, pin.utils.zero(rmodel.nv)])
 problem = crocoddyl.ShootingProblem(x0, [runningModel] * T, terminalModel)
- 
+
 # # Creating the DDP solver for this OC problem, defining a logger
 solver = crocoddyl.SolverDDP(problem)
 
-solver.setCallbacks([
-    crocoddyl.CallbackLogger(),
-    crocoddyl.CallbackVerbose(),
-])
+solver.setCallbacks(
+    [
+        crocoddyl.CallbackLogger(),
+        crocoddyl.CallbackVerbose(),
+    ]
+)
 
 # # Solving it with the DDP algorithm
 x = solver.solve()
- 
+
 # # Plotting the solution and the DDP convergence
 log = solver.getCallbacks()[0]
 
